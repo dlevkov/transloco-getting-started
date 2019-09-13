645ec0b22a7451d7773279722f561c4e6c939769
dlevkov <dlevkov@gmail.com>
2019-09-13 13:16:06 +0300

    add transloco

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
+import { TranslocoService } from '@ngneat/transloco';
+import { Subscription } from 'rxjs';
+import { take } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'transloco-getting-started';
-}
+  constructor(private service: TranslocoService) {}
+  changeLang(event) {
+  const lang = event.target.value;
+  this.service.setActiveLang(lang.toLowerCase());
+  }
+  }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,31 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { environment } from '../environments/environment';
+import { translocoLoader } from './transloco.loader';
+import {
+  TranslocoModule,
+  TRANSLOCO_CONFIG,
+  TranslocoConfig,
+} from '@ngneat/transloco';
 
 @NgModule({
-  declarations: [
-    AppComponent
+  declarations: [AppComponent],
+  imports: [BrowserModule, HttpClientModule, TranslocoModule],
+  providers: [
+    {
+      provide: TRANSLOCO_CONFIG,
+      useValue: {
+        listenToLangChange: true,
+        defaultLang: 'en',
+        fallbackLang: 'es',
+        prodMode: environment.production,
+        scopeStrategy: 'shared',
+      } as TranslocoConfig,
+    },
+    translocoLoader,
   ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
